bdd5017814743f952286c6c8eab02c88b914a799
Jakees <sqi.1139160137@qq.com>
2016-06-22 21:53:58 +0800

    添加了ButterKnife注解。。。

--- app/src/main/java/com/widget/sql/pulltorefreshlistview/activity/MainActivity.java
+++ app/src/main/java/com/widget/sql/pulltorefreshlistview/activity/MainActivity.java
@@ -1,36 +1,33 @@
 package com.widget.sql.pulltorefreshlistview.activity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 import com.widget.sql.pulltorefreshlistview.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by SQL on 2016/6/22.
  */
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.btn_pull_to_refresh_list_view)
+    Button btnPullToRefreshListView;
 
-    Button button;
+    @OnClick(R.id.btn_pull_to_refresh_list_view)
+    public void onClick() {
+        startActivity(new Intent(MainActivity.this, PullToRefreshListViewActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.btn_pull_to_refresh_list_view);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PullToRefreshListViewActivity.class));
-            }
-        });
+        ButterKnife.bind(this);
 
     }
-
-
-
 }
